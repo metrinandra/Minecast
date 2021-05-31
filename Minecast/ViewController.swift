6501ef7fa6692d90f364203cee89b7c9fe269bb4
metrinandra <48483843+metrinandra@users.noreply.github.com>
2021-05-31 13:09:25 +0300

    Initial Commit

---
+++ Minecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minecast
+//
+//  Created by Andra Metrina on 31/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
